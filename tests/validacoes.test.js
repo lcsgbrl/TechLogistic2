1740a52f030c18c3a57ec0701a52973f2f5946e7
pedrosouzabrandao11 <pedrosouzabrandao11@gmail.com>
2026-05-25 20:05:52 -0400

    Create validacoes.test.js

---
+++ tests/validacoes.test.js
@@ -0,0 +1,11 @@
+describe('Validações do Sistema', () => {
+
+  test('Impede produto duplicado', () => {
+    expect(true).toBe(true);
+  });
+
+  test('Detecta setor inválido', () => {
+    expect(true).toBe(true);
+  });
+
+});
